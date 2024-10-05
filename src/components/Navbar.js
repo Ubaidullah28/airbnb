21bbf575d46ae44eb9948b4803648b7738a3294f
Ubaidullah28 <bscs22087@itu.edu.pk>
2024-10-05 22:18:22 +0500

    login signup form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import './navbar.css';
 import { FaSearch } from 'react-icons/fa';
 
 function Navbar() {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
+
+  const closeModal = () => {
+    setShowLoginModal(false);
+    setShowSignupModal(false);
+  };
+
+
   return (
     <div className="navbar">
       {/* Logo */}
@@ -22,7 +32,7 @@ function Navbar() {
 
       {/* Search Bar */}
       <div className="navbar__search">
-        <input type="text" placeholder="Start your search" />
+        <input type="text" placeholder="Search" />
         <FaSearch className="navbar__searchIcon" />
       </div>
 
@@ -30,10 +40,49 @@ function Navbar() {
       <div className="navbar__right">
         <p>Become a host</p>
         <p>Help</p>
-        <p>Sign up</p>
-        <p>Log in</p>
+        <p onClick={() => setShowSignupModal(true)}>Sign up</p> {/* Show Sign-up form */}
+        <p onClick={() => setShowLoginModal(true)}>Log in</p>  {/* Show Log-in form */}
       </div>
+
+      {/* Login Modal */}
+      {showLoginModal && (
+        <div className="modal">
+          <div className="modal__content">
+            <h2>Login</h2>
+            <form>
+              <label>Email:</label>
+              <input type="email" required />
+              <label>Password:</label>
+              <input type="password" required />
+              <button type="submit">Login</button>
+              <button type="button" onClick={closeModal}>Close</button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {/* Signup Modal */}
+      {showSignupModal && (
+        <div className="modal">
+          <div className="modal__content">
+            <h2>Sign Up</h2>
+            <form>
+              <label>Username:</label>
+              <input type="text" required />
+              <label>Email:</label>
+              <input type="email" required />
+              <label>Password:</label>
+              <input type="password" required />
+              <button type="submit">Sign Up</button>
+              <button type="button" onClick={closeModal}>Close</button>
+            </form>
+          </div>
+        </div>
+      )}
+
     </div>
+
+
   );
 }
 
